868f9459e73f5e537d161ec44642465620f29a44
“DanielRoblero” <mr.roblero@gmail.com>
2020-09-17 07:36:59 -0700

    Update templates and put some basic HTML

---
+++ app.py
@@ -0,0 +1,33 @@
+from flask import Flask, render_template, request, url_for
+
+app = Flask(__name__)
+
+posts = [
+    {
+        'author': 'Corey Schafer',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'April 20, 2018'
+    },
+    {
+        'author': 'Jane Doe',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'April 21, 2018'
+    }
+]
+
+@app.route('/')
+@app.route('/home')
+def home():
+    return render_template('home.html', posts=posts)
+
+@app.route('/about')
+def about():
+    return render_template('about.html', title='ABOUT PAGE')
+
+
+
+# python app.py name is main if we run the script directly.If we run somewhere else name will be our module name
+if __name__ == '__main__':
+    app.run(debug=True)
